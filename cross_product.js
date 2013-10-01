64a265b72e8aa7222c3252e74014a18d0edbd2bb
Hanuse <sarbortova@wisc.edu>
2013-10-01 00:41:27 -0500

    add cross_product

--- cross_product.js
+++ cross_product.js
@@ -4,9 +4,9 @@ module.exports = function(vector1, vector2) {
 	var result = vector1[0]*vector2[1] - vector1[1]*vector2[0];
   }else if(vector1.length == 3 && vector2.length == 3){
 	var result = new Array(3);
-	result[0] = 0;
-	result[1] = 1;
-	result[2] = 2;
+	result[0] = vector1[1]*vector2[2] - vector1[2]*vector2[1];
+	result[1] = vector1[2]*vector2[0] - vector1[0]*vector2[2];
+	result[2] = vector1[0]*vector2[1] - vector1[1]*vector2[0];
   }else{
 	var result =  null;
   }
